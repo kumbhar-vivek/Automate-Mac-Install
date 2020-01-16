5f5a86483a284aea1197b2d739fce02f8b4fcb85
kumbhar-vivek <kumbhar.vivek@outlook.com>
2020-01-16 17:46:29 +0530

    Update coreinstall.sh

--- coreinstall.sh
+++ coreinstall.sh
@@ -26,7 +26,7 @@ echo "Configure Git..."
 touch .gitconfig
 git config -l --global
 git config --global user.name "< user-name >" # add github account
-git config --global user.email < github-email > # add github email
+git config --global user.email "< email-id >" # add github email
 
 echo "Check Git configuration..."
 git config -l --global
@@ -36,9 +36,6 @@ ruby -v
 
 echo "Install Ruby..."
 brew install ruby
-
-# echo "Linking Ruby..."
-# brew unlink ruby && brew link ruby
 
 echo "Check Gem Manager..."
 gem -v
@@ -51,35 +48,25 @@ echo "gem: --no-document" >> ~/.gemrc
 
 brew doctor
 
-brew tap homebrew/cask-fonts
-
-fonts=(
-  font-ibm-plex
-  font-fira-code
-)
-
-echo "Install Fonts..."
-brew cask install ${fonts[@]}
-
-brew tap home-brew/cask-cask
+brew tap homebrew/cask
 
 apps=(
+  1password
   android-file-transfer
-  atom
-  box-drive
   docker
   free-download-manager
-  freemind
   firefox
   github
-  google-backup-and-sync
-  google-chrome
   handbrake
   imageoptim
   inkscape
   iterm2
-  slack
+  the-unarchiver
+  tutanota
+  tor-browser
+  visual-studio-code
   vlc
+  xmind
   xquartz
 )
 
@@ -96,24 +83,28 @@ brewapps=(
 echo "Installing Brew Apps..."
 brew install ${brewapps[@]}
 
-
 echo ""
 echo "Enter AppleID to signin to Mac App Store.."
-read -p " AppleID (< apple_id >): " APPLEID # add Apple ID here
+read -p "AppleID: " APPLEID # add Apple ID here
 
 mas signin $APPLEID
 
 masapps=(
-  880001334 # Reeder (3.2.1)
-  1147396723 # WhatsApp (0.3.1242)
-  425424353 # The Unarchiver (4.0.0)
+  880001334 # Reeder (3.2.3)
+  985367838 # Microsoft Outlook (16.32)
+  823766827 # OneDrive (19.192.0926)
+  497799835 # Xcode (11.3)
+  784801555 # Microsoft OneNote (16.32)
+  462062816 # Microsoft PowerPoint (16.32)
+  462054704 # Microsoft Word (16.32)
+  462058435 # Microsoft Excel (16.32)
 )
 
 mas install ${masapps[@]}
 
 echo "Upgrading installed brews..."
 
-brew update
+brew upgrade
 
 echo "Removing all the cask downloads..."
 brew cask cleanup --prune 0
@@ -124,22 +115,54 @@ rm -rf $(brew --cache)
 brew cleanup --prune 0
 brew doctor
 
-# echo "Add brewski to .bash_profile..."
-# touch ~/.bash_profile
-# echo "export PATH=/usr/local/opt/ruby/bin:$PATH" >> ~/.bash_profile
-# echo "alias brewski='mas outdated && mas upgrade && brew update && brew upgrade && brew cask upgrade && brew cleanup --prune 0; brew doctor'" >> ~/.bash_profile
+echo "macOS configs..."
 
-echo "Install Atom packages..."
-apm install --packages-file ~/Documents/atomConfig/atom-packages.txt
-cp -f ~/Documents/atomConfig/config.cson ~/.atom/config.cson
-cp -f ~/Documents/atomConfig/styles.less ~/.atom/styles.less
+defaults write "NSGlobalDomain" "AppleShowAllExtensions" -bool TRUE
+defaults write "com.apple.desktopservices" "DSDontWriteNetworkStores" -bool TRUE
 
-echo "Disable .DS_Store and ._ file on Network or USB..."
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool TRUE
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+defaults write "com.apple.finder" "AppleShowAllFiles" -bool TRUE
+defaults write "com.apple.finder" "ShowStatusBar" -bool TRUE
+defaults write "com.apple.finder" "ShowPathbar" -bool TRUE
+defaults write "com.apple.finder" "ShowHardDrivesOnDesktop" -bool TRUE
+defaults write "com.apple.finder" "ShowExternalHardDrivesOnDesktop" -bool TRUE
+defaults write "com.apple.finder" "ShowRecentTags" -bool FALSE
+defaults write "com.apple.finder" "DownloadsFolderListViewSettingsVersion" -bool TRUE
+defaults write "com.apple.finder" "NSWindowTabbingShoudShowTabBarKey-com.apple.finder.TBrowserWindow" -bool TRUE
+defaults write "com.apple.finder" "FXPreferredViewStyle" -string "clmv"
+defaults write "com.apple.finder" "FXDefaultSearchScope" -string "SCcf"
+defaults write "com.apple.finder" "NewWindowTarget" -string "PfLo"
+defaults write "com.apple.finder" "NewWindowTargetPath" -string "file:///Users/kumbhar-vivek/Downloads/"
 
-echo "Show hidden files..."
-defaults write com.apple.finder AppleShowAllFiles -bool TRUE
+defaults write "com.apple.dock" "tilesize" -int 30
+defaults write "com.apple.dock" "show-recents" -bool FALSE
+defaults write "com.apple.dock" "autohide" -bool FALSE
+defaults write "com.apple.dock" "show-process-indicators" -bool TRUE
+defaults write "com.apple.dock" "magnification" -bool TRUE
+defaults write "com.apple.dock" "launchanim" -bool TRUE
+defaults write "com.apple.dock" "minimize-to-application" -bool TRUE
+defaults write "com.apple.dock" "largesize" -int 128
+defaults write "com.apple.dock" "mineffect" -string "scale"
+
+defaults write "com.apple.AppleMultitouchTrackpad" "TrackpadThreeFingerDrag" -bool TRUE
+defaults write "com.apple.menuextra.battery" "ShowPercent" -bool TRUE
+defaults write "com.apple.menuextra.clock" "DateFormat" -string "EEE d MMM  h:mm:ss a"
+
+/usr/libexec/PlistBuddy -c "set :DesktopViewSettings:IconViewSettings:iconSize 40" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "set :DesktopViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "set :DesktopViewSettings:IconViewSettings:showItemInfo TRUE" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "set :DesktopViewSettings:IconViewSettings:labelOnBottom FALSE" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "set :DesktopViewSettings:IconViewSettings:showIconPreview TRUE" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 40" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:showItemInfo TRUE" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:showIconPreview TRUE" ~/Library/Preferences/com.apple.finder.plist
+
+sudo defaults write /Library/Preferences/com.apple.commerce "AutoUpdate" -bool TRUE
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate "AutomaticallyInstallMacOSUpdates" -bool TRUE
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate "AutomaticCheckEnabled" -bool TRUE
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate "AutomaticDownload" -bool TRUE
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate "CriticalUpdateInstall" -bool TRUE
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate "ConfigDataInstall" -bool TRUE
 
 echo "Resetting LaunchPad..."
 defaults write com.apple.dock ResetLaunchPad -bool TRUE;killall Dock
