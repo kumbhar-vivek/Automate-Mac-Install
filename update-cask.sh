782df7160316e88ed950b7473bf4a4c3b930710e
kumbhar-vivek <kumbhar.vivek@outlook.com>
2020-01-16 17:47:27 +0530

    Update update-cask.sh

--- update-cask.sh
+++ update-cask.sh
@@ -1,7 +1,6 @@
 brew update     # download app updated formulas
-brew cask upgrade
+brew upgrade
 brew outdated   # what’s old?
-brew cleanup --prune 0	# clean brew
-brew cask cleanup	--prune 0 # cleanup cask
+brew cleanup	--prune 0 # cleanup cask
 brew doctor	# verify brew
 defaults write com.apple.dock ResetLaunchPad -bool TRUE;killall Dock
